4bc7ec1b49a1af02af5225c61ea89fdda4a59d2b
Anthony Cornehl <accornehl@gmail.com>
2012-10-30 20:31:07 -0700

    Modifications to config.mk

--- config.mk
+++ config.mk
@@ -4,25 +4,31 @@ VERSION = 6.1
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+PREFIX = ${HOME}
+MANPREFIX =
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib
 
 # Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
+XINERAMALIBS = -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
 # includes and libs
 INCS = -I${X11INC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS}
 
-# flags
-CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = -s ${LIBS}
+# System flags
+CFLAGS = -O2 -fomit-frame-pointer -pipe
+CPPFLAGS = -O2 -fomit-frame-pointer -pipe
+LDFLAGS = -Wl,-O1 -Wl,--sort-common -Wl,--as-needed
+
+# Build flags
+CPPFLAGS += -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CFLAGS += -g -std=c99 -pedantic -Wall ${INCS} ${CPPFLAGS}
+#CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS += -g ${LIBS}
+#LDFLAGS = -s ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
